9ccfad0a616c6426f99c92d8a9371a277146f3c1
ykrop23 <70780982+ykrop23@users.noreply.github.com>
2021-04-18 15:26:53 +0300

    Add files via upload

---
+++ AddCouple.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TimeTable.ViewModel;
+
+namespace TimeTable.View
+{
+    /// <summary>
+    /// Логика взаимодействия для AddCouple.xaml
+    /// </summary>
+    public partial class AddCouple : UserControl
+    {
+        public AddCouple()
+        {
+            InitializeComponent();
+            DataContext = new AddCoupleViewModel();
+        }
+    }
+}
