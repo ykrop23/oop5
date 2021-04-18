c2de127af5de210cc746615c8f2da7ac9604f3dc
ykrop23 <70780982+ykrop23@users.noreply.github.com>
2021-04-18 15:32:10 +0300

    Add files via upload

---
+++ Couple.cs
@@ -0,0 +1,16 @@
+﻿namespace TimeTable.Model
+{
+    public class Couple
+    {
+        public string Name { get; set; }
+        public string GroupNumber { get; set; }
+        public string Faculty { get; set; }
+        public string Data { get; set; }
+        public string DayOfWeak { get; set; }
+        public int CoupleNumber { get; set; }
+        public string TypeWork { get; set; }
+        public string Type { get; set; }
+        public int Count { get; set; }
+
+    }
+}
